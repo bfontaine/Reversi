f2b06582650243cd9c8a2d9045a012f058995b45
Baptiste Fontaine <batifon@yahoo.fr>
2012-04-24 12:47:39 +0200

    run_tests.sh now show how many assertions have been successfully passed

--- run_tests.sh
+++ run_tests.sh
@@ -4,6 +4,8 @@ for f in tests/*_tests.c;do
 
     tmp_f=${f%%.c}
     tmp_f=${tmp_f##tests/}
+
+    assert_nb=$(grep -c "assert(" ${f})
     
     tmp_err=$(tempfile)
     rm -f $tmp_err
@@ -25,4 +27,7 @@ for f in tests/*_tests.c;do
         rm -f $tmp_err
         exit -1
     fi
+
+    echo "${assert_nb} assertions successfully passed."
+
 done 2> /dev/null
