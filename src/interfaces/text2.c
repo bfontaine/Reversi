2518fd33e68bf42fd6c964a32ff535ecb6bfdaf9
Baptiste Fontaine <batifon@yahoo.fr>
2012-05-18 19:34:23 +0200

    text2 interface uses a better read_command(…) function

--- src/interfaces/text2.c
+++ src/interfaces/text2.c
@@ -1,8 +1,8 @@
-#include <stdio.h>
-#include "interface.h"
-
 #ifndef _TEXT_2_C
 #define _TEXT_2_C 1
+
+#include <stdio.h>
+#include "interface.h"
 
 #define COMMAND_PROMPT "reversi> "
 
@@ -17,14 +17,49 @@ int close_interface() {
 
 
 int print_board(board *b) {
-    
-    int r=MAX_SQ, c;
+
+    int r=MAX_SQ, c, pretty_c = 8;
+    char sq;
+
+    if (BOARD_PRETTY_PRINT) {
+        fputs("   A B C D E F G H\n", stderr);
+    }
 
     for (; r>=MIN_SQ; r--) {
-        for (c=MIN_SQ; c<=MAX_SQ; c++) {
-            printf(" %c", b->game_board[c][r]);
+        if (BOARD_PRETTY_PRINT) {
+            fprintf(stderr, "%d ", pretty_c--);
         }
-        puts("");
+        for (c=MIN_SQ; c<=MAX_SQ; c++) {
+            sq = b->game_board[c][r];
+            if (BOARD_COLORS == 0) {
+
+                if (sq == BLACK_C) {
+                    sq = FRENCH_BLACK_C;
+                }
+                else if (sq == WHITE_C) {
+                    sq = FRENCH_WHITE_C;
+                }
+
+                fprintf(stderr, " %c", sq);
+            } else {
+                /* colors */
+                if (sq == BLACK_C) {
+                    fprintf(stderr, BOARD_COLOR_B, FRENCH_BLACK_C);
+                }
+                else if (sq == WHITE_C) {
+                    fprintf(stderr, BOARD_COLOR_W, FRENCH_WHITE_C);
+                }
+                else if ((r+c)%2 == 1) {
+                    sq = (BOARD_PRINT_EMPTY) ? sq : ' ';
+                    fprintf(stderr, BOARD_COLOR_1, sq);
+                }
+                else if ((r+c)%2 == 0) {
+                    sq = (BOARD_PRINT_EMPTY) ? sq : ' ';
+                    fprintf(stderr, BOARD_COLOR_2, sq);
+                }
+            }
+        }
+        fprintf(stderr, "\n");
     }
 
     return 0;
@@ -39,21 +74,19 @@ int print_score(int score) {
 
 int print_moves(char** moves, int moves_number) {
     
-    if (!moves_number) { return 0; }
-
-    printf("%s", moves[0]);
-
-    if (moves_number == 1) {
-        puts(".");
+    if (!moves_number) {
+        puts("COUPS");
         return 0;
     }
 
-    int i=1;
+    printf("COUPS");
+
+    int i=0;
 
     for (; i<moves_number; i++) {
-        printf(", %s", moves[i]);
+        printf(" %2s", moves[i]);
     }
-    puts(".");
+    puts("");
 
     return 0;
 }
@@ -62,16 +95,14 @@ int print_moves(char** moves, int moves_number) {
 int print_winner(char winner) {
 
     if (winner == WHITE_C) {
-        printf("%s won\n", WHITE_NAME);
+        puts("BLANC gagne.");
         
     } else if (winner == BLACK_C) {
-        printf("%s won\n", BLACK_NAME);
+        puts("NOIR gagne.");
 
     } else if (winner == TIE_C) {
-        puts("Tie");
+        puts("Egalite.");
     
-    } else { /* should not happen */
-        puts("Chuck Norris won");
     }
 
     return 0;
@@ -80,7 +111,7 @@ int print_winner(char winner) {
 
 int print_error(char* error_str) {
 
-    printf("ERROR %s\n", error_str);
+    printf("ERREUR %s\n", error_str);
     return 0;
 }
 
@@ -94,31 +125,25 @@ int print_notice(char* notice_str) {
 
 int read_command(char** command) {
 
-    char* newline, c = '\0';
+    if (*command == NULL) {
+        return -21;
+    }
+    /* we assume that strlen(*command) >= CMD_MAX_SIZE */
 
-    fflush(stdin);
-    printf(COMMAND_PROMPT);
-    if (fgets(*command, sizeof *command, stdin) == NULL) {
-        return 1;
-    }
-        
-    newline = strchr(*command, '\n');
-    if (newline != NULL) {
-        /* if there is a newline */
-        *newline = '\0';
-    } else {
-        /* if there is no newline */
-        puts("");
-    }
+    char c = fgetc(stdin);
+    int len = 0;
 
-    /* flush stdin */
-    while (c != '\n' && c != EOF) {
-        c = getchar();
+    while ((c != EOF) && (c != '\n') && (len+1 < CMD_MAX_SIZE)) {
+        if (len+1 < CMD_MAX_SIZE) {
+            *((*command)+(len++)) = c;
+        }
+
+        c = fgetc(stdin);
     }
+    *((*command)+len) = '\0';
 
     return 0;
 }
-
 
 
 #endif /* _TEXT_2_C */
