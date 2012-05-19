caaf97a08258cd8056b6f4717e1a15c9eecc61ec
Baptiste Fontaine <batifon@yahoo.fr>
2012-05-19 16:54:55 +0200

    T_PLAY renammed in T_MOVE

--- tests/game_tests.c
+++ tests/game_tests.c
@@ -73,104 +73,104 @@ int main(void) {
      *	SCW: score for white player
      *	SCB: score for black player
      */
-#define T_PLAY(P,S,SC,SCW,SCB) assert(play(b,P,S) == SC); \
+#define T_MOVE(P,S,SC,SCW,SCB) assert(play(b,P,S) == SC); \
     assert(get_score(b, WHITE_C) == SCW); \
     assert(get_score(b, BLACK_C) == SCB); \
     assert(get_piece(b, S, &piece) == 0); \
     assert(piece == P)         
     
     /* black plays in E6*/
-    T_PLAY(BLACK_C,"E6",2,1,4);
+    T_MOVE(BLACK_C,"E6",2,1,4);
 
     /* white plays in F4*/
-    T_PLAY(WHITE_C,"F4",2,3,3);
+    T_MOVE(WHITE_C,"F4",2,3,3);
 
     /* black plays in E3*/
-    T_PLAY(BLACK_C,"E3",2,2,5);
+    T_MOVE(BLACK_C,"E3",2,2,5);
 
     /* white plays in F6*/
-    T_PLAY(WHITE_C,"F6",2,4,4);
+    T_MOVE(WHITE_C,"F6",2,4,4);
 
     /* black plays in F5*/
-    T_PLAY(BLACK_C,"F5",2,3,6);
+    T_MOVE(BLACK_C,"F5",2,3,6);
 
     /* white plays in D6*/
-    T_PLAY(WHITE_C,"D6",4,7,3);
+    T_MOVE(WHITE_C,"D6",4,7,3);
 
     /* black plays in C6*/
-    T_PLAY(BLACK_C,"C6",2,6,5);
+    T_MOVE(BLACK_C,"C6",2,6,5);
 
     /* white plays in G5*/
-    T_PLAY(WHITE_C,"G5",2,8,4);
+    T_MOVE(WHITE_C,"G5",2,8,4);
 
     /* black plays in G6*/
-    T_PLAY(BLACK_C,"G6",5,4,9);
+    T_MOVE(BLACK_C,"G6",5,4,9);
 
     /* white plays in C5*/
-    T_PLAY(WHITE_C,"C5",2,6,8);
+    T_MOVE(WHITE_C,"C5",2,6,8);
 
     // the board is not full
     assert(is_full(b) == 0);
 
     /* black plays in C4*/
-    T_PLAY(BLACK_C,"C4",4,3,12);
+    T_MOVE(BLACK_C,"C4",4,3,12);
 
     /* white plays in G7*/
-    T_PLAY(WHITE_C,"G7",3,6,10);
+    T_MOVE(WHITE_C,"G7",3,6,10);
 
     /* black plays in H8 */ 
-    T_PLAY(BLACK_C,"H8",4,3,14);
+    T_MOVE(BLACK_C,"H8",4,3,14);
 
     /* white plays in C7 */
-    T_PLAY(WHITE_C,"C7",3,6,12);
+    T_MOVE(WHITE_C,"C7",3,6,12);
 
     /* black plays in G4 */
-    T_PLAY(BLACK_C,"G4",4,3,16);
+    T_MOVE(BLACK_C,"G4",4,3,16);
 
     /* white plays in G3 */
-    T_PLAY(WHITE_C,"G3",2,5,15);
+    T_MOVE(WHITE_C,"G3",2,5,15);
 
     /* black plays in F3 */
-    T_PLAY(BLACK_C,"F3",2,4,17);
+    T_MOVE(BLACK_C,"F3",2,4,17);
 
     /* white plays in B4 */
-    T_PLAY(WHITE_C,"B4",2,6,16);
+    T_MOVE(WHITE_C,"B4",2,6,16);
 
     /* black plays in G2 */
-    T_PLAY(BLACK_C,"G2",2,5,18);
+    T_MOVE(BLACK_C,"G2",2,5,18);
 
     /* white plays in H2 */
-    T_PLAY(WHITE_C,"H2",3,8,16);
+    T_MOVE(WHITE_C,"H2",3,8,16);
 
     /* black plays in H3 */
-    T_PLAY(BLACK_C,"H3",2,7,18);
+    T_MOVE(BLACK_C,"H3",2,7,18);
 
     /* white plays in H6 */
-    T_PLAY(WHITE_C,"H6",5,12,14);
+    T_MOVE(WHITE_C,"H6",5,12,14);
 
     /* black plays in H7 */
-    T_PLAY(BLACK_C,"H7",2,11,16);
+    T_MOVE(BLACK_C,"H7",2,11,16);
 
     /* white plays in H4 */
-    T_PLAY(WHITE_C,"H4",3,14,14);
+    T_MOVE(WHITE_C,"H4",3,14,14);
 
     /* black plays in H5 */
-    T_PLAY(BLACK_C,"H5",4,11,18);
+    T_MOVE(BLACK_C,"H5",4,11,18);
 
     /* white plays in F2 */
-    T_PLAY(WHITE_C,"F2",6,17,13);
+    T_MOVE(WHITE_C,"F2",6,17,13);
 
     /* black plays in H1 */
-    T_PLAY(BLACK_C,"H1",6,12,19);
+    T_MOVE(BLACK_C,"H1",6,12,19);
 
     /* white plays in C3 */
-    T_PLAY(WHITE_C,"C3",2,14,18);
+    T_MOVE(WHITE_C,"C3",2,14,18);
 
     /* black plays in A4 */
-    T_PLAY(BLACK_C,"A4",4,11,22);
+    T_MOVE(BLACK_C,"A4",4,11,22);
 
     /* white plays in D3 */
-    T_PLAY(WHITE_C,"D3",3,14,20);
+    T_MOVE(WHITE_C,"D3",3,14,20);
 
     /* TODO continue game until one player can pass his turn
      * TODO continue game until one player won */
